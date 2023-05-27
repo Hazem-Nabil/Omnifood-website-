01a4f7f98b6171b51b48b9baf40245e4dfb5eb21
Hazem Nabil <96664832+Hazem-Nabil@users.noreply.github.com>
2023-05-27 23:47:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/** @format */
+
+"use-strict";
+
+// // const greet = function (gretting) {
+// //   return function (name) {
+// //     console.log(`${gretting} ${name}`);
+// //   };
+// // };
+
+// // const hay = greet("Hay");
+// // hay("Hazem");
+// // hay("Ahmed");
+
+// const greet = (greeting) => (name) => {
+//   console.log(`${greeting} ${name}`);
+// };
+
+// // const hay = greet("Hay");
+// // hay("Laila");
+// // hay("Hazem");
+
+// greet("Hi")("laila Hazem");
